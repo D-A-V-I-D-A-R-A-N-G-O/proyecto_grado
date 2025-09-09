681567b0043471bce6976f8211f77786d7759201
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2025-09-09 14:37:58 -0500

    Rename .html files to .php and update internal links
    Renamed student-related HTML files to PHP extensions and updated all internal navigation links to use the new .php filenames. This prepares the project for future PHP integration and ensures consistent navigation.

--- clse/html/estudiantes/tarjetas.php
+++ clse/html/estudiantes/tarjetas.php
@@ -7,14 +7,13 @@
     <link rel="stylesheet" href="../../css/navegacion.css">
 </head>
 <body>
-    <header>
         <header id="cabeza">
-            <img id="logo" src="../../img/image.png" alt="">
+             <img id="logo" src="../../img/image.png" alt="">
        <div id="menu">
-        <a id="boton" href="./inicio.html">Inicio</a>
-        <a id="boton" href="./tarjetas.html">Tarjetas</a>
-        <a id="boton" href="./crear.html">Crear</a>
-        <a id="out" href="../../index.html"><img id="salir" src="../../img/logout.jpg" alt=""></a>
+        <a id="boton" href="./inicio.php">Inicio</a>
+        <a id="boton" href="./tarjetas.php">Tarjetas</a>
+        <a id="boton" href="./crear.php">Crear</a>
+        <a id="out" href="../../index.php"><img id="salir" src="../../img/logout.jpg" alt=""></a>
         </div>
 </header>
    
