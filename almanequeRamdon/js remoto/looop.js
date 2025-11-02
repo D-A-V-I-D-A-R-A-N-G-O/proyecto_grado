b6b6a007bdaae8c06e99029dee8b306d54d3c92a
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2025-11-02 03:37:05 -0500

    purga

--- almanequeRamdon/js remoto/looop.js
+++
@@ -1,7 +0,0 @@
-function pepe() {
-    let pepe =2221
-while (pepe > 1) {
-    pepe= pepe++
-    console.log(pepe)
-}
-}
--- almanequeRamdon/js/looop.js
+++
@@ -1,7 +0,0 @@
-function pepe() {
-    let pepe =2221
-while (pepe > 1) {
-    pepe= pepe*pepe
-    console.log(pepe)
-}
-}
